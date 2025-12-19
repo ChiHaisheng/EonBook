29291ea46b2f55c058cb8211f8520b003442439a
zlchs1718 <148679471+zlchs1718@users.noreply.github.com>
2025-12-19 09:48:17 +0800

    Delete assets directory

--- assets/folders/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as FolderIcon_1 } from "./Folder_1.svg";
-export { default as FolderIcon_2 } from "./Folder_2.svg";
-export { default as FolderIcon_3 } from "./Folder_3.svg";
-export { default as FolderIcon_4 } from "./Folder_4.svg";
-export { default as FolderIcon_5 } from "./Folder_5.svg";
-export { default as FolderIcon_6 } from "./Folder_6.svg";
-export { default as FolderIcon_7 } from "./Folder_7.svg";
-export { default as FolderIcon_8 } from "./Folder_8.svg";
